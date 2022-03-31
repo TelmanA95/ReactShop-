087147051290ceeb361137a7bf5fc6208d744ea0
TelmanA95 <alekyant77@mail.ru>
2022-04-01 01:29:39 +0400

    add titles

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect } from "react";
-import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
 import { useDispatch, useSelector } from "react-redux";
 import {
   getProdAsync,
   selectProd,
 } from "../../Redux/productSlice/productSlice";
-import "../Products/Products.scss"
+import "../Products/Products.scss";
 export default function Products() {
   const prods = useSelector(selectProd);
   const dispatch = useDispatch();
@@ -16,7 +16,21 @@ export default function Products() {
   }, []);
 
   return (
-    <div className="mainProd">
+    <div className="prods">
+<div className="titleStore">
+        <h2>The Store</h2>
+        <div className="line"></div>
+        <div className="storeTxt">
+          <span>
+            Choose well and buy less. We believe in owning fewer, quality things
+            and making them last a long time. That’s why if any of our product
+            ever breaks down, you can send it back to us and we’ll repair it.
+          </span>
+      </div>
+
+        </div>
+        <div className="mainProd">
+      
       {prods?.map((item) => {
         return (
           <div className="product">
@@ -33,10 +47,11 @@ export default function Products() {
               <h3>{item.title}</h3>
               <p>${item.price}</p>
             </div>
-            
           </div>
         );
       })}
     </div>
+    </div>
+   
   );
 }
